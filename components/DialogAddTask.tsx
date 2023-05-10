b43b1e0a8a1b9ba55b31ea2ac0c104f3fae5efdb
Miguel Aguilera <superbrandon2018@gmail.com>
2023-05-10 15:43:55 -0500

    Validation Form Add Task

--- components/DialogAddTask.tsx
+++ components/DialogAddTask.tsx
@@ -1,12 +1,18 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function DialogAddTask({onOpen, setOpen}:any) {
+    const [title, setTitle] = React.useState<string>("")
+    const [description, setDescription] = React.useState<string>("")
+    const [addDisabled, setAddDisabled] = React.useState<boolean>(true);
 
     const handleClose = () => {
         setOpen(false);
     };
-
+    useEffect(() => {
+        (title.length !== 0 && description.length !== 0) ? setAddDisabled(false) : setAddDisabled(true);
+        
+    },[title,description])
     return (
         <Dialog open={onOpen} onClose={handleClose}>
             <DialogTitle className='text-semibold'>!Proactividad!</DialogTitle>
@@ -20,6 +26,8 @@ export default function DialogAddTask({onOpen, setOpen}:any) {
                     label="Nombre de la actividad"
                     fullWidth
                     variant="outlined"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
                 />
                 <TextField
                     margin="dense"
@@ -29,10 +37,12 @@ export default function DialogAddTask({onOpen, setOpen}:any) {
                     label="Descripcion"
                     fullWidth
                     variant="outlined"
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description}
                 />
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose}>Agregar</Button>
+                <Button disabled={addDisabled} onClick={handleClose}>Agregar</Button>
             </DialogActions>
         </Dialog>
     )
--- components/DialogFriendTransfer.tsx
+++ components/DialogFriendTransfer.tsx
@@ -1,32 +1,36 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, InputAdornment, InputLabel, OutlinedInput, TextField } from '@mui/material'
 import React from 'react'
 
-export default function DialogFriendTransfer({onOpen, setOpen}:any) {
-    
+import QrCodeIcon from '@mui/icons-material/QrCode';
+
+export default function DialogFriendTransfer({ onOpen, setOpen }: any) {
+
     const handleClose = () => {
         setOpen(false);
     };
-  
+
     return (
-    <Dialog open={onOpen} onClose={handleClose}>
+        <Dialog open={onOpen} onClose={handleClose}>
             <DialogTitle className='text-semibold'>Friend Transfer</DialogTitle>
             <DialogContentText className='px-6'>
                 En esta seccion puedes configurar a quien le enviaras tus solanas
                 si no cumples con tus objetivos
             </DialogContentText>
             <DialogContent>
-            
-                <TextField
-                    margin="dense"
-                    id="name"
-                    label="Direccion de solana"
-                    fullWidth
-                    variant="outlined"
-                />
+                <FormControl fullWidth>
+                    <InputLabel htmlFor="outlined-adornment-amount">Friend Direction</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-qr"
+                        endAdornment={<InputAdornment position="end">
+                            <QrCodeIcon/>
+                        </InputAdornment>}
+                        label="qr"
+                    />
+                </FormControl>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose}>Agregar</Button>
             </DialogActions>
         </Dialog>
-  )
+    )
 }
